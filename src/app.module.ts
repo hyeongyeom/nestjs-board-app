a64c000c80efae6663a72d2dcc10db4706982975
hyeongyeom <wltnwkd244@naver.com>
2022-10-06 21:36:11 +0900

    refactor: custom repository decorator

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import {TypeOrmModule} from '@nestjs/typeorm'
 import { BoardsModule } from './boards/boards.module';
 import { typeORMConfig } from './boards/configs/typeorm.config';
+import { TypeOrmExModule } from './boards/db/typeorm-ex.module';
+import {BoardRepository} from './boards/board.repository'
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(typeORMConfig),
-    BoardsModule]
+  TypeOrmModule.forRoot(typeORMConfig),
+    TypeOrmExModule.forCustomRepository([BoardRepository]),
+    BoardsModule
+  ]
 })
 export class AppModule {}
--- src/boards/board.repository.ts
+++ src/boards/board.repository.ts
@@ -1,1 +1,8 @@
-export class BoardRepository extends Repository
+import { Repository } from 'typeorm';
+import { Board } from './board.entity';
+import { CustomRepository } from './db/typeorm-ex.decorator';
+
+@CustomRepository(Board)
+export class BoardRepository extends Repository<Board> {
+
+}
