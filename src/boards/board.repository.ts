ee8a77cdeab4b28a282d1d3974d5e669ab3297e1
hyeongyeom <wltnwkd244@naver.com>
2022-10-06 22:59:19 +0900

    refactor: modified logic with repository

--- src/boards/board.repository.ts
+++ src/boards/board.repository.ts
@@ -1,8 +1,26 @@
 import { Repository } from 'typeorm';
+import { BoardStatus } from './board-status.enum';
 import { Board } from './board.entity';
 import { CustomRepository } from './db/typeorm-ex.decorator';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @CustomRepository(Board)
 export class BoardRepository extends Repository<Board> {
 
-}
+    async CreateBoard(createBoardDto:CreateBoardDto) : Promise<Board> {
+        const {title,description} = createBoardDto
+
+        const board=this.create({
+            title,
+            description,
+            status:BoardStatus.PUBLIC
+        })
+
+        await this.save(board)
+        return board
+    }
+
+   
+
+    }
+
--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { get } from 'http';
 import { title } from 'process';
-import { Board, BoardStatus } from './board.model';
+import { BoardStatus } from './board-status.enum';
 import { BoardsService } from './boards.service';
+import { Board } from './board.entity';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
@@ -11,31 +12,26 @@ import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe'
 export class BoardsController {
     constructor(private boardsService:BoardsService) {}
 
-    @Get('/') 
-    getAllBoard(): Board[] {
+    @Get()
+    getAllBoards() : Promise<Board[]> {
         return this.boardsService.getAllBoards();
     }
-    
+
     @Post()
     @UsePipes(ValidationPipe)
-    createBoard(@Body() createBoardDto:CreateBoardDto ):Board {
-    return this.boardsService.createBoard(createBoardDto);
+    createBoard(@Body() createBoardDto:CreateBoardDto):Promise<Board> {
+        return this.boardsService.createBoard(createBoardDto);
     }
-
     @Get('/:id')
-    getBoardById(@Param('id') id:string):Board {
+    getBoardById(@Param('id') id:number) : Promise<Board> {
         return this.boardsService.getBoardById(id);
     }
     @Delete('/:id')
-    deleteBoard(@Param('id') id:string):void {
-        this.boardsService.deleteBoard(id);
-    } 
-
+    deleteBoard(@Param('id',ParseIntPipe) id:number):Promise<void> {
+        return this.boardsService.DeleteBoard(id);
+    }
     @Patch('/:id/status')
-    updateBoardStatus(
-        @Param('id') id:string,
-        @Body('status',BoardStatusValidationPipe) status:BoardStatus
-    ) {
+    updateBoardStatus(@Param('id',ParseIntPipe) id:number,@Body('status',BoardStatusValidationPipe) status:BoardStatus) : Promise<Board> {
         return this.boardsService.updateBoardStatus(id,status);
     }
 }
